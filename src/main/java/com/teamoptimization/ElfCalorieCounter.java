d93f96e26bf76101a7704b8e71390ddb79a78da5
Christos18 <christosd18@gmail.com>
2024-04-23 11:20:01 +0100

    Passing two elfs one item each test.

--- src/main/java/com/teamoptimization/ElfCalorieCounter.java
+++ src/main/java/com/teamoptimization/ElfCalorieCounter.java
@@ -1,21 +1,27 @@
 package com.teamoptimization;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class ElfCalorieCounter {
     public ElfCalorieCounter() {
 
     }
 
-    public Integer calculateMax(String calories) {
-        String[] caloriesArray = calories.split("\n\n");
+    public int calculateMax(String calories) {
+        String[] caloriesArray = calories.trim().split("\n\n");
+        int max = 0;
 
-        for (String calorie : caloriesArray) {
-            return Integer.valueOf(calorie);
+        for (String calorie: caloriesArray) {
+            String[] caloriesPerElf = calorie.trim().split("\n");
+            int totalPerElf = 0;
+
+            for (String individualCalorie : caloriesPerElf) {
+                totalPerElf += Integer.parseInt(individualCalorie);
+            }
+
+            if (totalPerElf > max) {
+                max = totalPerElf;
+            }
         }
 
-        return 0;
+        return max;
     }
 }
--- src/test/java/com/teamoptimization/ElfCalorieCounterTest.java
+++ src/test/java/com/teamoptimization/ElfCalorieCounterTest.java
@@ -1,6 +1,5 @@
 package com.teamoptimization;
 
-import com.oocode.Example;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -31,8 +30,30 @@ public class ElfCalorieCounterTest {
                         10000"""
         ), equalTo(24000));
     }
+
     @Test
     public void checkMaxCaloriesForOneElfOneItem() {
         assertThat(underTest.calculateMax("1000"), equalTo(1000));
+    }
+
+    @Test
+    public void checkMaxCaloriesForTwoElfOneItemEach() {
+        assertThat(underTest.calculateMax(
+                """
+                1000
+                
+                2000
+                """), equalTo(2000));
+    }
+    @Test
+    public void checkMaxCaloriesForTwoElvesTwoItemsEach() {
+        assertThat(underTest.calculateMax(
+                """
+                1000
+                3000
+                
+                2000
+                500
+                """), equalTo(4000));
     }
 }
